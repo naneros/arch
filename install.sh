b8a60928e085b7592da81f6f667ea330bb4d6c8a
naneros <naneros21@gmail.com>
2020-11-01 09:57:42 +0100

    update

--- install.sh
+++ install.sh
@@ -18,8 +18,20 @@ sudo pacman -S rsync
 sudo pacman -S jdk8-openjdk
 sudo pacman -S java8-openjfx
 sudo pacman -S mc
-sudo pacman -S ranger
 sudo pacman -S awesome awesome-terminal-fonts
+sudo pacman -S wl-clipboard
+sudo pacman -S htop
+sudo pacman -S base-devel
+sudo pacman -S lxsession
+
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
+cd ..
+
+mkdir -p ~/.config/kak/plugins/
+git clone https://github.com/robertmeta/plug.kak.git ~/.config/kak/plugins/plug.kak
+
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
